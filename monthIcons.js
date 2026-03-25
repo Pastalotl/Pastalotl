024bb0f1d6df022019bb0d1b6b6b077bd6150872
Silaera <123200732+Pastalotl@users.noreply.github.com>
2026-03-25 23:24:58 +0000

    fleshed out some pages (correct repo)

--- monthIcons.js
+++ monthIcons.js
@@ -4,16 +4,13 @@ let month = today.getMonth()+1
 let year = today.getFullYear()
 
 let icon = "../icons/"
-iconList = ["Swanopoly Idle.png","Ifforrem Idle.png","Automata Combat.png","Kauplaire Idle.png","Ddwgyl Idle.png","Ddwgyl Idle (Pride).png","Vayens Idle.png","Taure Idle.png","Calian Idle.png","Nigelas Idle.png","Silaera Idle.png","Nigelas Festive.png"]
-prideIconList = ["Ddwgyl Idle (Pride).png","Silaera Idle (Pride)", "Kauplaire Idle (Pride)", "Taure Idle (Pride)", "Calian Idle (Pride)"]
+iconList = ["Swanopoly Idle.png","Ifforrem Idle.png","Peter Idle.png","Kauplaire Idle.png","Ddwgyl Idle.png","Ddwgyl Idle (Pride).png","Vayens Idle.png","Taure Idle.png","Calian Idle.png","Nigelas Idle.png","Silaera Idle.png","Nigelas Festive.png"]
+prideIconList = ["Ddwgyl Idle (Pride).png","Silaera Idle (Pride).png", "Kauplaire Idle (Pride).png", "Taure Idle (Pride).png", "Calian Idle (Pride).png"]
 festiveIconList = ["Nigelas Festive.png"]
 if(month==3 && date==30){/*international day of trans visibility*/
     icon += "Silaera Idle (Pride).png"
 }
-if(month==3&&date==14){/*pi day*/
-    icon += "Calian Combat.png"
-}
-else if(month==5){ /*pride month*/
+else if(month==6){ /*pride month*/
     icon += prideIconList(Math.floor(Math.random()*(prideIconList.length)))
 }
 else if(month==12){
